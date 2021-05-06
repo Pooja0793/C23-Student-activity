e8a83dbc1213fbd5675653df221d1ef9b36041bb
Pooja0793 <72892373+Pooja0793@users.noreply.github.com>
2021-05-06 13:57:43 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -4,12 +4,14 @@ var options ={
     isStatic : true
 }
         this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
         World.add(myworld,this.body);
     }
 
     display(){
         var pos = this.body.position;
         rectMode(CENTER);
-        rect(pos.x,pos.y,50,50);
+        rect(pos.x,pos.y,this.width,this.height);
     }
 }
