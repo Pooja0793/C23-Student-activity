546ccaf9bf74aa81f088e81dd2c7302e83c94d25
Pooja0793 <72892373+Pooja0793@users.noreply.github.com>
2021-05-18 14:40:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 
-var myengine,myworld;
+var engine,world;
 var box1;
 var box2;
 
@@ -10,19 +10,21 @@ function setup() {
   createCanvas(400,400);
   //createSprite(400, 200, 50, 50);
 
-  myengine = Engine.create();
+  engine = Engine.create();
   //myworld is myengine's world
-  myworld = myengine.world;
+  world = engine.world;
 
   box1 = new Box(200,100,50,50);
+    box2 = new Box(240,100,50,100);
   console.log(box1);
 
-  Engine.run(myengine);
+  Engine.run(engine);
 }
 
 function draw() {
   background(255,255,255);  
   box1.display();
+  box2.display();
   
   //drawSprites();
 }
